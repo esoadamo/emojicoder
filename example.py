fbc817fb145eb65597517bb615c2ee0f002ed6b4
Adam Hlaváček <esoadamo@gmail.com>
2017-07-22 10:41:46 +0200

    optimize runtime decode function

--- example.py
+++ example.py
@@ -3,21 +3,14 @@
 """
 THIS FILE WAS ENCODED BY EMOJICODER
 https://github.com/esoadamo/emojicoder
-
-::CODE::
-😜😇😉😩😛😧😐😨😉😶😡😥😛😆😱😯😈😇😝😯😜😦😱😤😉😲😤🙀
 """
 from base64 import b64decode as ee
-from codecs import open as o
+c = "😚😖😵😰😛😷😉😴😈😇😑😩😛😖😔😍😂😠😴😊😜😇😉😩😛😧😐😨😈😤😡😥😛😆😱😯😋😢😈😩😃😐😩😰😜😦😥😮😝😂😠😢😐😶😽😵😛😧😑😩" \
+    "😛😦😜😠😙😧😉😯😛😒😀😱😈😇😑😯😈😃😄😰😈😢😤😍😂😦😙😯😜😢😁😩😈😆😥😮😈😇😉😡😛😦😝😥😊😃😄😬😈😃😄😱😊😓😨😍😂😢😀😠" \
+    "😈😂😁😰😜😦😥😮😝😂😡😩😊😐😴😊😈😂😀😠😈😇😑😩😛😖😔😮😜😶😱😥😙😗😀😨😌😒😤😍😂😧😁😲😚😖😹😴😊😂😝😃😛😶😽😬😋😂😁😩" \
+    "😜😶😹😜😉😷😐😠😚😗😐😿😉😲😤😍😂😠🙀🙀"
 e = list("😀😁😂😃😄😅😆😇😈😉😊😋😌😍😎😏😐😑😒😓😔😕😖😗😘😙😚😛😜😝😞😟😠😡😢😣😤😥😦😧😨😩😪😫😬"
          "😭😮😯😰😱😲😳😴😵😶😷😸😹😺😻😼😽😾😿🙀🙁🙂🙃🙄🙅🙆🙇🙈🙉🙊🙋🙌🙍🙎🙏")
 b = list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=")
-with o(__file__, 'r', 'utf8') as f:
-    c = False
-    for l in f.read().splitlines():
-        if c:
-            exec(str(ee(''.join([b[e.index(ch)] for ch in list(l)])), 'utf8'))
-            break
-        elif l.startswith('::CODE::'):
-            c = True
-# prints "hello world"
+exec(str(ee(''.join([b[e.index(ch)] for ch in list(c)])), 'utf8'))
+
